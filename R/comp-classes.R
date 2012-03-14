2b40325c475ded1c3fe2ebffc6e538c7be6043d3
tengfei <tengfei@tengfei-ThinkPad-T510>
2012-03-14 16:32:17 -0500

    add IntegerWithRange

--- R/comp-classes.R
+++ R/comp-classes.R
@@ -88,12 +88,12 @@ NonpositiveInteger <- function(object){
   new("NonpositiveInteger", object)
 }
 
-## virtual class
+## virtual class NumericWithRange
 setClass("NumericWithRange", contains = c("VIRTUAL"),
          representation(min = "numeric",
                        max = "numeric"))
 
-setNumericWithRange <- function(prefix, min, max, where = topenv(parent.frame())){
+setNumericWithRange <- function(prefix = "Numeric", min, max, where = topenv(parent.frame())){
   cls <- paste(prefix, "With", "Min", min, "Max", max, sep = "")
   setClass(cls, contains = c("NumericWithRange", "numeric"),
            prototype = prototype(min = min, max = max),
@@ -105,3 +105,23 @@ setNumericWithRange <- function(prefix, min, max, where = topenv(parent.frame())
 
 ## NumericWithMin0Max1
 setNumericWithRange("Numeric", min = 0, max = 1)
+
+setClass("IntegerWithRange", contains = c("VIRTUAL"),
+         representation(min = "integer",
+                        max = "integer"))
+
+setIntegerWithRange <- function(prefix = "Integer", min, max, where = topenv(parent.frame())){
+  cls <- paste(prefix, "With", "Min", min, "Max", max, sep = "")
+  setClass(cls, contains = c("IntegerWithRange", "integer"),
+           prototype = prototype(min = min, max = max),
+           validity = function(object){
+             if(is.na(object) | !is.integer(object)){
+                paste("values must be integer")
+             }else if(object<min | object >max){
+               paste("values must be within", min, "and", max)
+             }
+           }, where = where)
+}
+
+
+
